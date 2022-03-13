bf0751d9adfc9463f5140eaaed633c9f1929adb9
Eugene-36 <67147219+Eugene-36@users.noreply.github.com>
2022-03-13 19:11:51 +0200

    added main part

--- Tile.js
+++ Tile.js
@@ -11,9 +11,25 @@ export default class Tile {
     this.value = value;
   }
 
+  get value() {
+    return this.#value;
+  }
+
   set value(v) {
     this.#value = v;
     this.#tileElement.textContent = v;
+    const power = Math.log2(v);
+    const backgroundLightness = 100 - power * 9;
+
+    this.#tileElement.style.setProperty(
+      '--background-lightness',
+      `${backgroundLightness}%`
+    );
+
+    this.#tileElement.style.setProperty(
+      '--text-lightness',
+      `${backgroundLightness <= 50 ? 90 : 10}%`
+    );
   }
 
   set x(value) {
@@ -24,5 +40,20 @@ export default class Tile {
   set y(value) {
     this.#y = value;
     this.#tileElement.style.setProperty('--y', value);
+  }
+  remove() {
+    this.#tileElement.remove();
+  }
+
+  waitForTransition(animation = false) {
+    return new Promise((resolve) => {
+      this.#tileElement.addEventListener(
+        animation ? 'animationed' : 'transitionend',
+        resolve,
+        {
+          once: true,
+        }
+      );
+    });
   }
 }
